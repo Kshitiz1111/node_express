9299359e6c586af20d3303e815382ee75ca22bf2
kshitizghimire8@gmail.com <kshitizghimire8@gmail.com>
2023-01-11 21:47:03 +0800

    passing all static files using middle ware including index.html as a rood file

--- express/server.js
+++ express/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 //all the static files stays in public folder
 //setup static and middleware
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-app.get('/',(req, res)=>{
-   res.sendFile(path.resolve(__dirname,'./navbar/index.html'))
-});
+// app.get('/',(req, res)=>{
+//    res.sendFile(path.resolve(__dirname,'./navbar/index.html'))
+// });
 
 app.all('*',(req, res)=>{res.status(404).send("page not found!!!")})
 
